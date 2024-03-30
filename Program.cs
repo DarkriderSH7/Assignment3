f66a146aab0e95ecc4760fdcf36a4590e1a4d7af
DarkriderSH7 <suhaib.hussain26000@gmail.com>
2024-03-29 23:53:58 -0400

    second commit

--- Program.cs
+++ Program.cs
@@ -3,16 +3,14 @@ using Assignment3.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+
 builder.Services.AddControllersWithViews();
 
-// Configure EF Core to use SQL Server.
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
